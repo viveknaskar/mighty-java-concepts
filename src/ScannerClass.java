583ef424f305ad0c6b9c47bbede971cca06d7f12
Vivek Naskar <viveknaskar2912@gmail.com>
2019-09-15 14:26:38 +0530

    Scanner class demonstration (for taking user input)

---
+++ src/ScannerClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ScannerClass {
+
+    // Scanner class to take input from user
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while (scan.hasNext()) {
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
+
