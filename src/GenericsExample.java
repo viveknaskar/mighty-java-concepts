bc6e1dd81d1d3a5dabfc99376005733f0475c7e0
Vivek Naskar <viveknaskar2912@gmail.com>
2020-03-19 01:06:32 +0530

    A example of generics class of user-defined objects

---
+++ src/GenericsExample.java
@@ -0,0 +1,37 @@
+/*
+* A simple program to demonstrate user-defined generics
+*
+*  Generics allow type (Integer, String, … etc and user defined types) to be a parameter to methods, classes and interfaces.
+* */
+public class GenericsExample {
+
+    public static void main(String[] args) {
+
+        UserDefinedGenericClass<Integer> intObj = new UserDefinedGenericClass<>(100);
+        System.out.println("*****Generics instance type for Integer****** " + intObj.getObj());
+
+        UserDefinedGenericClass<Double> doubleObj = new UserDefinedGenericClass<>(100.0);
+        System.out.println("*****Generics instance type for Double****** " + doubleObj.getObj());
+
+        UserDefinedGenericClass<String> stringObj = new UserDefinedGenericClass<>("Generics is awesome");
+        System.out.println("*****Generics instance type for String****** " + stringObj.getObj());
+
+    }
+
+}
+
+
+class UserDefinedGenericClass<T> {
+    // An object of type T
+    T obj;
+
+    // Constructor
+     UserDefinedGenericClass(T obj) {
+        this.obj = obj;
+    }
+
+    // Getter method
+    public T getObj() {
+        return obj;
+    }
+}
