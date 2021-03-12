691855284a82b1787dda5219d1e6c0dbc9489574
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-12 22:28:49 +0530

    Optimization

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,13 +1,8 @@
 // Java program to Reverse a String by converting string to characters one by one
 
-import java.lang.*;
-import java.io.*;
-import java.util.*;
+public class ReverseString {
 
-class ReverseString
-{
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String str = "JAVA IS AWESOME";
 
         // convert String to character array by using toCharArray
